6617e214b871faa9d104c6484927f2b669c1864d
alex11920 <av61799@gmail.com>
2024-03-28 19:19:10 +0600

    finished

--- src/components/children/Hero.jsx
+++ src/components/children/Hero.jsx
@@ -4,15 +4,14 @@ const Hero = () => {
   return (
     <div className="hero min-h-screen bg-base-200 rounded-3xl mt-12 mb-24 py-10 lg:py-0">
       <div className="hero-content flex-col lg:flex-row-reverse gap-24">
-        <img
-          src="public\images\banner.png"
-          className="max-w-sm"
-        />
+        <img className="max-w-sm" src="https://i.ibb.co/tbFt7pL/banner.png" alt="" />
         <div className="max-w-[526px] text-center lg:text-start">
           <h1 className="fonter text-5xl font-bold pb-12">
             Books to freshen up your bookshelf
           </h1>
-          <Link to="/listedbooks"><button className="btn mainBg text-white">View The List</button></Link>
+          <Link to="/listedbooks">
+            <button className="btn mainBg text-white">View The List</button>
+          </Link>
         </div>
       </div>
     </div>
